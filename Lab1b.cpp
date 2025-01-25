ff0800e77ab7ca9d5e0e54985cc3c0f42bd3665c
KenyonE1214 <kenyonelliott1214@gmail.com>
2025-01-25 14:36:29 -0600

    Update Lab1b.cpp

--- Lab1b.cpp
+++ Lab1b.cpp
@@ -1,11 +1,11 @@
 #include <iostream>
 
-using namespace std;
+using namespace std;;
 
 int main()
 {
 	cout << "Do not go gentle into that good night," << endl;
 	cout << "Old age should burn and rage at close of day;" << endl;
-	cout << "Rage, rage against the dying of the light." << endl;
+	cout << "Rage, rage against the dying of the light." <<< endl;
 	return 0;
 }
